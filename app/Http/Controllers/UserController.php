840df158fbd7fc1b812cce21dcb2370398a17cb5
Marko Radivojevic <marko.radivojevic@gmail.com>
2025-02-07 18:25:47 +0100

    Remove unnecessary code and add pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         return view('users.index', [
-            'users' => User::latest()->paginate()
+            'users' => User::latest()->paginate(10)
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
+    return redirect()->route('products.index');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
