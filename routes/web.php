ccbe0aeb4a235121a63e32756bca87743e5c6a7d
Marko Radivojevic <marko.radivojevic@gmail.com>
2025-02-07 18:29:36 +0100

    Add redirection from dashboard to product list page

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::get('/', function () {
     return redirect()->route('products.index');
 });
 
+Route::get('/dashboard', function () {
+    return redirect()->route('products.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
