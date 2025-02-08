eb08f8d88739ffd304f461b9833fa23d9bcc45c6
Marko Radivojevic <marko.radivojevic@gmail.com>
2025-02-08 15:02:59 +0100

    Clean up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 
                 <!-- Success Message -->
                 @if (session('success'))
-                    <div class="max-w-7xl mx-auto mt-4 px-4 sm:px-6 lg:px-8 ">
+                    <div class="max-w-7xl mx-auto mt-4 px-4 sm:px-6 lg:px-8">
                         <div class="bg-green-100 border border-green-400 px-4 py-3 rounded relative" role="alert">
                             <strong class="font-bold">Success!</strong> {{ session('success') }}
                         </div>
