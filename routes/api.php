4eb2a99f92de420793038aa4c1cf1235bf9ec59a
Marko Radivojevic <marko.radivojevic@gmail.com>
2025-02-08 09:55:36 +0100

    Clean up routes

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Api\V1\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{category}',    [CategoryController::class, 'show']);
 
--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('users', UserController::class)->middleware(AdminMiddleware::class);
     Route::resource('categories', CategoryController::class)->middleware(AdminMiddleware::class);
     Route::resource('products', ProductController::class)->middleware(AdminMiddleware::class);
+    Route::delete('/product-images/{id}', [ProductImageController::class, 'destroy'])->name('product-images.destroy')->middleware(AdminMiddleware::class);
     Route::resource('comments', CommentController::class)->only(['index', 'edit', 'update', 'destroy']);
-
-    Route::delete('/product-images/{id}', [ProductImageController::class, 'destroy'])->name('product-images.destroy')->middleware(AdminMiddleware::class);;
 });
 
 require __DIR__.'/auth.php';
